5df3ba31a11863f2a9a8c390dfa07ec52f7c8c20
Yeongju Kang <yeongjukang@outlook.com>
2020-05-11 12:14:51 +0900

    added description
    descriptions added
    
    description changes

--- static_files/README.md
+++ static_files/README.md
@@ -1,15 +1,22 @@
 # Static Files
 
-Example to creates an atreugo static files server.
+Example to create an atreugo static file server with basic middleware configuration.<br/>
+If you want to see subdirecories and their files served, execute commands below.
+
+```bash
+mkdir -p nested/one
+echo 'Nested Directory' > ./nested/dir.txt
+echo 'One More' > ./nested/one/more.txt
+```
 
 ### Routes:
-
+- `/`
 - `/main`
 - `/readme`
-- `/license`
+- `/gitignore`
 
 - `/static/default`
 - `/static/middlewares`
 - `/static/custom`
 - `/static/readme`
-- `/static/license`
+- `/static/gitignore`
--- static_files/main.go
+++ static_files/main.go
@@ -2,7 +2,25 @@ package main
 
 import (
 	"github.com/savsgio/atreugo/v11"
+	"github.com/savsgio/go-logger/v2"
 )
+
+var httpRespBody string = `
+<h1>Atreugo Static File Example</h1>
+<h2>Links are ordered by func main's code flow.</h2>
+
+<p>Directory or file mapped directly</p>
+<a href="/main">Directory mapped at /main</a><br/>
+<a href="/readme">README.md on /readme</a></br>
+<a href="/gitignore">.gitignore on /gitignore, with middlewares configured</a></br>
+
+<p>Static group with prefix /static</p>
+<a href="/static/default/">To /static/default</a><br/>
+<a href="/static/middlewares/">To /static/middlewares, with middlewares configured</a><br/>
+<a href="/static/custom">You will see forbidden here on /static/custom</a><br/>
+<a href="/static/readme">README.md on /static/readme</a></br>
+<a href="/static/gitignore">.gitignore on /static/gitignore, with middlewares configured</a></br>
+`
 
 func main() {
 	config := atreugo.Config{
@@ -22,6 +40,12 @@ func main() {
 		After:  []atreugo.Middleware{afterView},
 	}
 
+	server.GET("/", func(ctx *atreugo.RequestCtx) error {
+		defer logger.Info("\t\tview's defer")
+
+		return ctx.HTTPResponse(httpRespBody, 200)
+	}).Middlewares(middlewares)
+
 	// Serve files with default configuration
 	server.Static("/main", "./")
 
@@ -29,7 +53,7 @@ func main() {
 	server.ServeFile("/readme", "README.md")
 
 	// Serve just one file with middlewares
-	server.ServeFile("/license", "LICENSE").Middlewares(middlewares)
+	server.ServeFile("/gitignore", ".gitignore").Middlewares(middlewares)
 
 	// Creates a new group to serve static files
 	static := server.NewGroupPath("/static")
@@ -52,7 +76,7 @@ func main() {
 	static.ServeFile("/readme", "README.md").UseBefore(beforeView)
 
 	// Serve just one file with middlewares
-	static.ServeFile("/license", "LICENSE").Middlewares(middlewares)
+	static.ServeFile("/gitignore", ".gitignore").Middlewares(middlewares)
 
 	// Run
 	if err := server.ListenAndServe(); err != nil {
